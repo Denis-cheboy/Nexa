c54a58934a13be3547d4fb0d23cb7b6ef03e5609
Denis Onyango <denocheboy@gmail.com>
2025-06-26 22:56:49 +0300

    added lived origin to be allowed to reach our server

--- backend_app/main.py
+++ backend_app/main.py
@@ -13,7 +13,7 @@ app = FastAPI(
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:3000"], # Add your React app's URL
+    allow_origins=["http://localhost:5173", "http://localhost:3000","https://nexa-1-b2ub.onrender.com"], # Add your React app's URL
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
